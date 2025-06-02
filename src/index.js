2943dba9dbd2002973052e708cc4e3388fd409c0
Ana Chatillon <a_chantil@hotmail.com>
2025-06-02 21:24:25 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//Challenge 1
+let button1Element = document.querySelector("#challenge-1");
+button1Element.setAttribute("disabled", "disabled");
+
+//Challenge 2
+let button2Element = document.querySelector("#challenge-2");
+button2Element.style.background = "green";
+//Challenge 3
+let button3Element = document.querySelector("#challenge-3");
+button3Element.setAttribute("class", "danger");
